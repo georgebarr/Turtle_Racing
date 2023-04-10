72ff1e2aed4e84c5f86fc2bab18795ff5cf48a4d
George <110062762+georgebarr@users.noreply.github.com>
2023-04-10 16:23:43 +0100

    Added streak system

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ os.system('cls' if os.name == 'nt' else 'clear')
 screen = Screen()
 screen.setup(width=500, height=400)
 
+with open('streak.txt', 'r') as file:
+    streak = int(file.read())
+    file.close()
+
 colors = ['red', 'orange', 'black', 'green', 'blue', 'purple']
 
 prediction = screen.textinput(title="Make a prediction",
@@ -42,8 +46,14 @@ while race_occurring:
 
             if prediction == winner:
                 print(f"Your prediction was correct! The {winner} turtle won the race!")
+                streak += 1
             else:
                 print(f"Unfortunately, your prediction was incorrect. The {winner} turtle won the race.")
+                streak = 0
+
+            with open('streak.txt', 'w') as file:
+                file.write(str(streak))
+                file.close()
 
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
